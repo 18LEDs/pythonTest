d7cc5bc839d614feea8499b0872315b2b9f55b79
AgentBP77 <bryan.pierson@gmail.com>
2019-09-30 13:58:16 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,17 @@
 import sys
+import json
+import ast
+import configparser
+import urllib3
+import requests
+
+r = requests.get("http://www.google.com")
+print(r.status_code)
+
+config = configparser.ConfigParser()
+config.read("config")
+
+print(config["test"])
 
 test=sys.argv[1]
 print("Argument: " + test)
